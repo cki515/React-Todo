62ce4a516bede245dba911d06f2aa90e6617b44c
cki515 <kwangilstory@gmail.com>
2023-05-16 00:57:47 +1200

    Make React-Todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React, { useState, useRef, useEffect, useMemo } from "react";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  TextField,
+  Button,
+  Chip,
+  SwipeableDrawer,
+  List,
+  ListItem,
+  Divider,
+  Modal,
+  Snackbar,
+  Alert as MuiAlert,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function Sub4({ appNo }) {
+  const [no, setNo] = useState(0);
+  if (appNo % 2 != 0) {
+    return null;
+  }
+  return (
+    <>
+      <button onClick={() => setNo(no + 4)}>Sub4 : {no}</button>
+    </>
+  );
+}
+
+function Sub3({ appNo }) {
+  const [no, setNo] = useState(0);
+  if (appNo % 2 == 0) {
+    return null;
+  }
+  return (
+    <>
+      <button onClick={() => setNo(no + 3)}>Sub3 : {no}</button>
+    </>
+  );
+}
 
 function App() {
+  const [no, setNo] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setNo(no + 1)}>+1 : {no} </button>
+      <div>
+        <Sub3 appNo={no} />
+        <Sub4 appNo={no} />
+      </div>
+    </>
   );
 }
 
